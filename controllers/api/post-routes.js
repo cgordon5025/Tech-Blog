f0916e35cf79c0fe72a2f3cadc847bf7d39bff4c
Charlee Gordon <cgordon5025@gmail.com>
2022-10-25 20:14:15 -0400

    adding in comment and post routes

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -9,5 +9,31 @@ router.get('/', async (req, res) => {
     const posts = postData.map((data) => data.get({ plain: true }));
     res.json(posts)
 })
+//lets make a new post
+router.post('/', async (req, res) => {
+    try {
+        const postData = await Post.create({
+            post_title: req.body.post_title,
+            post_text: req.body.post_text,
+            user_id: req.body.user_id
+        })
+        res.json(postData)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+router.put('/:id', async (req, res) => {
+    try {
+        const postData = await Post.update(req.body, {
+            where: {
+                id: req.params.id
+            }
+        })
+        res.json(postData)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
 
 module.exports = router
